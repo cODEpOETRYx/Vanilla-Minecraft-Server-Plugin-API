4cfdddf9d07322da98d9b04b567841a2a6d71de4
cODEpOETRY <codepoetry123@gmail.com>
2016-12-21 13:24:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'rcon.php';
+include 'getlog.php';
+
+function mc.console($mc.console.cmd) {
+  return (rcon($mc.console.cmd));
+}
+
+function mc.cmd($mc.cmd.cmd,$mc.cmd.plugin) {
+  fopen()
+}
+
+?>
+  
